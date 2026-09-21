efa2d58130b7651d9652918307b4956b29d37b74
eumesmooliveira <deoliveiratech@gmail.com>
2026-09-21 16:28:23 -0400

    chore: improve Playwright configuration and test scripts

--- playwright.config.js
+++ playwright.config.js
@@ -26,10 +26,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://www.saucedemo.com',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace when retrying the failed test. */
     trace: 'on-first-retry',
   },
 
